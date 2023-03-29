3b75a3e303c4cc0debb0ffe1843243729bb32481
dosepehr <dosepehr@gmail.com>
2023-03-29 08:24:30 +0430

    make user model

---
+++ server/models/UserModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+});
+
+let User = mongoose.model('Users', UserSchema);
+
+export default User;
