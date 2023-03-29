54cef023565fe6c699cbbcf1a55f72588b96fd50
dosepehr <dosepehr@gmail.com>
2023-03-29 08:24:00 +0430

    make connection to db

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import usersRouter from './routes/UsersRoute.js';
 
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
 
-const server = express()
-server.use(cors())
+mongoose.connect('mongodb://127.0.0.1:27017/mern_crud');
 
+mongoose.Promise = global.Promise;
 
-
-
+server.use('/api/users', usersRouter);
 
 server.listen(4000, () => {
-    console.log('server is running on port 4000')
-})
-
-
-
-
+    console.log('server is running on port 4000');
+});
