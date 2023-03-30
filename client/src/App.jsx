c3f02a381970c7e088145ada461805e33469ca3f
dosepehr <dosepehr@gmail.com>
2023-03-31 00:49:34 +0430

    add users list to client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
+import UsersList from './components/UsersList';
 function App() {
-    return <h1 className='text-3xl font-bold underline bg-red-500'>Hello world!</h1>;
+    return (
+        <div>
+            <UsersList />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/UsersList.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const UsersList = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        const getUsers = async () => {
+            const { data } = await axios.get('http://localhost:4000/api/users');
+            setUsers(data);
+        };
+        getUsers();
+    }, []);
+    return (
+        <>
+            <div className='flex flex-col'>
+                <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                    <div className='py-2 inline-block min-w-full sm:px-6 lg:px-8'>
+                        <div className='overflow-hidden'>
+                            <table className='min-w-full'>
+                                <thead className='bg-white border-b'>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+                                        >
+                                            ID
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+                                        >
+                                            نام
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+                                        >
+                                            ایمیل
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+                                        >
+                                            جنسیت
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+                                        >
+                                            عملیات
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {users.length > 0 &&
+                                        users.map((item, index) => (
+                                            <tr
+                                                className='bg-gray-100 border-b'
+                                                key={index}
+                                            >
+                                                <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
+                                                    {index + 1}
+                                                </td>
+                                                <td className='text-sm text-gray-900  px-6 py-4 whitespace-nowrap'>
+                                                    {item.name}
+                                                </td>
+                                                <td className='text-sm text-gray-900  px-6 py-4 whitespace-nowrap'>
+                                                    {item.email}
+                                                </td>
+                                                <td className='text-sm text-gray-900  px-6 py-4 whitespace-nowrap'>
+                                                    {item.gender}
+                                                </td>
+                                                <td className='text-sm text-gray-900 space-x-4 px-6 py-4 whitespace-nowrap'>
+                                                 <button className='px-4 py-2 text-white bg-red-500 hover:bg-red-700 duration-200'>حذف</button>
+                                                 <button className='px-4 py-2 text-white bg-green-500 hover:bg-green-700 duration-200'>ویرایش</button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UsersList;
